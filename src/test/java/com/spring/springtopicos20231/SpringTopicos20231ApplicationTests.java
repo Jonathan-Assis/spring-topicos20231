a066157e6eb5650941d344da6eeaa9a4b3abd6c5
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-02-27 09:48:21 -0300

    :tada: initial commit

---
+++ src/test/java/com/spring/springtopicos20231/SpringTopicos20231ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.springtopicos20231;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringTopicos20231ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
