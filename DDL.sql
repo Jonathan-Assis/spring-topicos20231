af19e74d949560d73d6907c609ce41e1870508a3
Jonathan Gabriel <jonathan1999gabriel@gmail.com>
2023-03-07 13:30:50 +0000

    Criando tabela aotações

--- DDL.sql
+++ DDL.sql
@@ -1,15 +1,24 @@
-create schema anotacao;
+create schema if not exists anotacao;
 
 use anotacao;
 
-create user 'user'@'localhost' identified by 'pass123';
+create user if not exists 'user'@'localhost' identified by 'pass123';
 
 grant select, insert, delete, update on anotacao.* to user@'localhost';
 
-create table usr_usuario (
+create table if not exists usr_usuario (
   usr_id bigint unsigned not null auto_increment,
   usr_nome varchar(20) not null,
   usr_senha varchar(150) not null,
   primary key (usr_id),
   unique key uni_usuario_nome (usr_nome)
 );
+
+create table if not exists ant_anotacoes (
+  ant_id int not null auto_increment,
+  ant_anotacao not null varchar(256),
+  ant_data_criado not null datetime DEFAULT CURRENT_TIMESTAMP,
+  ant_data_atualizado datetime DEFAULT CURRENT_TIMESTAMP,
+  usr_id bigint unsigned not null,
+  constraint usr_usuario_anotacoes foreign key (usr_id) references usr_usuario(usr_id)
+)
